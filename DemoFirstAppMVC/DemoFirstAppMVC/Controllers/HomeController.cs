a0876b3b50b90f517106cd82433b1576ead8c524
hoangph <http://hoangph@222.252.27.89:8089/TNHS.git>
2017-07-17 17:16:48 +0700

    cm

---
+++ DemoFirstAppMVC/DemoFirstAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoFirstAppMVC.Models;
+
+namespace DemoFirstAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int get_hour = DateTime.Now.Hour;
+            ViewBag.Get_Hour = get_hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestRespone guest_respone)
+        {
+            return View("Thanks", guest_respone);
+        }
+    }
+}
