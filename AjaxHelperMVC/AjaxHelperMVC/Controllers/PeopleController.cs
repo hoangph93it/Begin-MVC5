dce6a781e056acc1d14c6c0fa125962c6eee660d
hoangph <http://hoangph@222.252.27.89:8089/TNHS.git>
2017-10-25 17:50:37 +0700

    ,

--- AjaxHelperMVC/AjaxHelperMVC/Controllers/PeopleController.cs
+++ AjaxHelperMVC/AjaxHelperMVC/Controllers/PeopleController.cs
@@ -10,31 +10,62 @@ namespace AjaxHelperMVC.Controllers
     {
         private Person[] personData = {
             new Person { ID = 1, Name = "Phan Huu Hoang", Gender = Genders.Mail, DOB = "18/08/1993", Address = "Vinh Tuong -  Vinh Phuc", Role = Roles.Admin },
-            new Person { ID = 2, Name = "Tran Kim Phuoc", Gender = Genders.Femail, DOB = "27/01/1997", Address = "Vinh Tuong -  Vinh Phuc", Role = Roles.User}
+            new Person { ID = 2, Name = "Tran Kim Phuoc", Gender = Genders.Femail, DOB = "27/01/1997", Address = "Vinh Tuong -  Vinh Phuc", Role = Roles.User},
+            new Person { ID = 3, Name = "Do Viet Thuong", Gender = Genders.Mail, DOB = "20/11/1993", Address = "Thieu Hoa -  Thanh Hoa", Role = Roles.Guest},
+            new Person { ID = 4, Name = "Hoang Trung Duc", Gender = Genders.Femail, DOB = "07/10/1994", Address = "Thieu Hoa -  Thanh Hoa", Role = Roles.Guest}
         };
         // GET: People
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult GetPeople()
+        //public ActionResult GetPeople()
+        //{
+        //    return View(personData);
+        //}
+        //[HttpPost]
+        //public ActionResult GetPeople(string selectedRole)
+        //{
+        //    if (selectedRole == null || selectedRole == "All")
+        //    {
+        //        return View(personData);
+        //    }
+        //    else
+        //    {
+        //        Roles selected = (Roles)Enum.Parse(typeof(Roles), selectedRole);
+        //        ViewData["Roleselected"] = selected;
+        //        return View(personData.Where(p => p.Role == selected));
+        //    }
+        //}
+        private IEnumerable<Person> GetData(string selectedRole)
         {
-            return View(personData);
-        }
-        [HttpPost]
-        public ActionResult GetPeople(string selectedRole)
-        {
-            if (selectedRole == null || selectedRole == "All")
+            IEnumerable<Person> data = personData;
+            if (selectedRole != "All")
             {
-                return View(personData);
+                Roles selected = (Roles)Enum.Parse(typeof(Roles), selectedRole);
+                data = personData.Where(p => p.Role == selected);
             }
-            else
+            return data;
+        }
+        public JsonResult GetPeopleDataJson(string selectedRole = "All")
+        {
+            IEnumerable<Person> data = GetData(selectedRole);
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public PartialViewResult GetPeopleData(string selectedRole = "All")
+        {
+            IEnumerable<Person> data = personData;
+            if (selectedRole != "All")
             {
                 Roles selected = (Roles)Enum.Parse(typeof(Roles), selectedRole);
                 ViewData["Roleselected"] = selected;
-                return View(personData.Where(p => p.Role == selected));
+                data = personData.Where(p => p.Role == selected);
             }
-
+            return PartialView(data);
+        }
+        public ActionResult GetPeople(string selectedRole = "All")
+        {
+            return View((object)selectedRole);
         }
     }
 }
