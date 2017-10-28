062e210585c1a459d6a1c8ac9236a85e2f335e31
hoangph <http://hoangph@222.252.27.89:8089/TNHS.git>
2017-10-28 15:24:37 +0700

    e

---
+++ Begin-MVC5/ModelBinding/ModelBinding/Controllers/PersonController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBinding.Models;
+
+
+namespace ModelBinding.Controllers
+{
+    public class PersonController : Controller
+    {
+        private Person[] dataPerson =
+        {
+            new Person {ID=1, Name="Phan Huu Hoang",DOB="18-08-1993",Gender= Genders.Mail, Role=Roles.Admin },
+            new Person {ID=2, Name="Tran Kim Phuoc",DOB="27-01-1997",Gender= Genders.Femail, Role=Roles.User },
+            new Person {ID=3, Name="Do Viet Thuong",DOB="02-02-1992",Gender= Genders.Mail,  Role=Roles.Guest },
+            new Person {ID=4, Name="Hoang Thi Duc",DOB="12-03-1993",Gender= Genders.Mail, Role=Roles.Guest }
+        };
+        // GET: Person
+        public ActionResult Index(int? id)
+        {
+            Person personItem = dataPerson.Where(p => p.ID == id).First();
+            return View(personItem);
+        }
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+        [HttpPost]
+        public ActionResult CreatePerson(Person model)
+        {
+            return View("Index", model);
+        }
+        public ActionResult DisplaySummary([Bind(Prefix = "Address"/*, Exclude = "City"*/)]AddressSummary summary)
+        {
+            return View(summary);
+        }
+        public ActionResult Name(string[] names)
+        {
+            names = names ?? new string[0];
+            return View(names);
+        }
+        public ActionResult Name(IList<string> names)
+        {
+            names = names ?? new List<string>();
+            return View(names);
+        }
+        public ActionResult Address(IList<AddressSummary> address)
+        {
+            address = address ?? new List<AddressSummary>();
+            return View(address);
+        }
+    }
+}
---
+++ Begin-MVC5/ModelBinding/ModelBinding/Models/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBinding.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DOB { get; set; }
+        public Genders Gender { get; set; }
+        public Addresss Address { get; set; }
+        public Roles Role { get; set; }
+    }
+    public enum Genders
+    {
+        Mail,
+        Femail
+    }
+    public enum Roles
+    {
+        Admin,
+        User,
+        Guest
+    }
+    public class Addresss
+    {
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+
+}
